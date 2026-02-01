94db9fce3db755880899f1f97da0efa37fbec735
SAIEED12 <abusaieed0@gmail.com>
2026-02-01 21:06:45 +0600

    first commit

---
+++ task1.js
@@ -0,0 +1,7 @@
+/* Write a function to convert temperature from Celsius to Fahrenheit. */
+
+function celsiusToFahren(tempC){
+    let result = tempC*1.8+32;
+    return result;
+}
+console.log(celsiusToFahren(30));
